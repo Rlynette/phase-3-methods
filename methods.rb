4734e1d9a064f53fd8db9f1415cc1d446c985b38
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-10-18 13:19:58 +0300

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,38 @@
 # Your code here!
+# function greetProgrammer() {
+#   console.log("Hello, programmer!");
+# }
+def greet_programmer
+   puts "Hello, programmer!"
+end
+greet_programmer
+# function greet(name) {
+#   console.log(`Hello, ${name}!`);
+# }
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+greet
+
+# function greetWithDefault(name = "programmer") {
+#   console.log(`Hello, ${name}!`);
+# }
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default
+
+# function add(num1, num2) {
+#   return num1 + num2;
+# }
+
+def add(num1, num2)  
+    num1 + num2
+end
+
+def halve(num)
+  if num.class != Integer
+    return nil
+  end
+  num/2
+end
